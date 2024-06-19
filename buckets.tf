9336c942153c05cd2a0098ba1bb58102ef82024f
charmingruby <mrrubycanal3105@gmail.com>
2024-06-19 14:49:56 -0300

    feat: base bucket config

---
+++ buckets.tf
@@ -0,0 +1,7 @@
+resource "aws_s3_bucket" "terraforming-bucket" {
+    bucket = "terraforming-capuccino-bucket"
+
+    tags = {
+        Name = "PDF Documents" // should have type of stuff stored
+    }   
+}
